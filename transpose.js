4e50200a65ad200b83ff97e0e48ad012ef965a60
Nunny R <nunnyreyes@gmail.com>
2021-03-25 11:12:58 -0400

    transpose matrix

--- transpose.js
+++ transpose.js
@@ -1,14 +1,15 @@
 class Solution {
     solve(matrix) {
         //switch the matrix and columns
-        for(let r = 0; r < matrix.length; r++){
-            console.log("r", r )
-            for(let c = 0; c < matrix.length; c++){
-                //console.log("c", c)
-                console.log("this", matrix[r][c])
-                matrix[r][c] = matrix[c][r]
+        //iterate through the row
+        let grid = []
+        for(let i = 0; i < matrix.length; i++){
+            grid.push([])
+            for(let j = 0; j < matrix.length; j++){
+               // console.log("cell", matrix[i][j])
+               grid[i].push(matrix[j][i])
             }
         }
-        return matrix
+        return grid
     }
 }
