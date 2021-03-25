c3fd3ea39c59d9dc79ddca154127ddbc8ea2de7d
Nunny R <nunnyreyes@gmail.com>
2021-03-25 11:40:08 -0400

    virtually tree

---
+++ virtuallyBinaryTree.js
@@ -0,0 +1,39 @@
+/**
+ * class Tree {
+ *   constructor(val, left=null, right=null) {
+ *     this.val = val
+ *     this.left = left
+ *     this.right = right
+ *   }
+ * }
+ */
+ class Solution {
+    solve(root, target) {
+        //n nodes whose values are 1 to n
+        //the root has a value of 1, its left child is 2 and 
+        //right is 3
+        //return whether target exists in root
+        //create helper function
+        let flag = false
+
+        function search(node,target){
+            //if there is no node then return. just stop
+            if(!node) return
+
+            if(node.val === target){
+               flag = true
+            }
+
+            //handle down the left side
+            if(node.left){
+                search(node.left, target)
+            }
+            //handle the right side
+            if(node.right){
+                search(node.right, target)
+            }
+        }
+        search(root, target)
+        return flag
+    }
+}
