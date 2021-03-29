7923e4cef7be4a110f6f0b7c4b19781728b2ebae
Nunny R <nunnyreyes@gmail.com>
2021-03-29 14:30:07 -0400

    confused

--- noisyPalindrome.js
+++ noisyPalindrome.js
@@ -5,7 +5,7 @@ class Solution {
         if(!onlyLow) return true //why am i returning true here
         //if string is empty then return true 
 
-        
+
         let i  = 0
         let back = onlyLow.length - 1
         //iteratively check them
@@ -21,3 +21,22 @@ class Solution {
         return true
     }
 }
+
+// class Solution {
+//     solve(matrix) {
+//     const grid = matrix.map(arr => arr.map(cell => 0))
+
+//     for(let i = 0; i < matrix.length; i++){
+//         for(let j = 0; j < matrix.length; j++){
+//             console.log("??", matrix[i][j])
+//             //console.log("grid", grid[i][j])
+//             grid[i][j] = matrix[i][j] + matrix[i][j]
+//            // grid[[j] += matrix[i - 1] + matrix[i]
+            
+            
+//             }
+//         }
+//         console.log("grid", grid)
+//      //   return grid
+//     }
+// }
