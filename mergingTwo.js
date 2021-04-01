6c3b8b3264334cc4e6e705bc304c7541f652ec2a
Nunny R <nunnyreyes@gmail.com>
2021-04-01 12:59:23 -0400

    merged

---
+++ mergingTwo.js
@@ -0,0 +1,14 @@
+class Solution {
+    solve(a, b) {
+        let final = []
+        
+ 
+        for(let char of a){
+            final.push(char)
+        }
+        for(let num of b){
+            final.push(num)
+        }
+        return final.sort((a,b) => a - b)
+    }
+}
