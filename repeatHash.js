ed468a76228c7d20f3937beab9f306bbed80e316
Nunny R <nunnyreyes@gmail.com>
2021-05-11 15:17:14 -0400

    edits

--- repeatHash.js
+++ repeatHash.js
@@ -1,23 +1,14 @@
-
 function lengthOfLongestSubstring(s){
     let hash = {}, 
         left=0,
         final = 0
     
     for(let index in s){
-        if(hash[(s[index]]){
-            left += hash[s[index]] + 1
+        if(hash[(s[index]] && index<left){
+          left += hash[s[index]] + 1   
         } 
         final = Math.max(final, index - left + 1) 
         hash[s[index]] = index
     }
     return final
   }
-  
-  dict = {
-    "a": 3,
-    "b": 1,
-    "c": 2
-  }
-  
-  
